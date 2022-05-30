d23bf5e224ef5ff2ef8c2e4e7868b3bc5e42493b
DeepGajjar7 <93926192+DeepGajjar7@users.noreply.github.com>
2022-05-30 11:18:26 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+leftwristx=0;
+rightwristx=0;
+difference=0;
+
 function setup(){
     video=createCapture(VIDEO)
     video.size(550,550)
@@ -10,6 +14,7 @@ function setup(){
 function draw() {
     background("#696969")
     fill("black")
+    textSize(difference)
     text("DEEP",250,250)
 }
 
@@ -20,5 +25,9 @@ function modelLoaded(){
 function gotposes(results){
     if (results.length>0){
         console.log(results)
+        leftwristx=results[0].pose.leftWrist.x;
+        rightwristx=results[0].pose.rightWrist.x;
+        difference=floor(leftwristx-rightwristx);
+
     }
 }
